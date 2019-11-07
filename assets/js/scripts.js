e570ab9d3e759cb64c686ce50f532b5f97454b0b
Bob Marteal <bobmarteal@gmail.com>
2019-11-07 14:00:07 -0500

    tons of work on gallery and new lessons images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,49 @@ jQuery(document).ready(function ($) {
         $(this.element).addClass('fadeIn');
         console.log('far out');
     }, {
-        offset: '40%'
+        offset: '80%'
     });
 
+    $(document).ready(function () {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function (item) {
+                    return item.el.attr('title') + '<small>photo by Miles Marteal</small>';
+                }
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('.big-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function (item) {
+                    return item.el.attr('title') + '<small>photo by Miles Marteal</small>';
+                }
+            }
+        });
+    });
+
+
 });
+
