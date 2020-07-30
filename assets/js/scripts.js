d32b02e15bbc57d0f14d85980842e1c117abbd0a
Bob Marteal <bobmarteal@gmail.com>
2020-07-29 23:43:32 -0400

    adds bikes listings, updates hoempage, adds civic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,23 +9,42 @@ jQuery(document).ready(function ($) {
     });
 
     $(document).ready(function () {
-        $('.popup-gallery').magnificPopup({
-            delegate: 'a',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                titleSrc: function (item) {
-                    return item.el.attr('title') + '<small>photo by Miles Marteal</small>';
-                }
+      $('.popup-gallery').each(function(){
+        $(this).magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          tLoading: 'Loading image #%curr%...',
+          mainClass: 'mfp-img-mobile',
+          gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+          },
+          image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+              return item.el.attr('title') + '<small>photo by Miles Marteal</small>';
             }
+          }
         });
+      });
+        // $('.popup-gallery').magnificPopup({
+        //     delegate: 'a',
+        //     type: 'image',
+        //     tLoading: 'Loading image #%curr%...',
+        //     mainClass: 'mfp-img-mobile',
+        //     gallery: {
+        //         enabled: true,
+        //         navigateByImgClick: true,
+        //         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        //     },
+        //     image: {
+        //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        //         titleSrc: function (item) {
+        //             return item.el.attr('title') + '<small>photo by Miles Marteal</small>';
+        //         }
+        //     }
+        // });
     });
 
     $(document).ready(function () {
